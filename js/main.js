e5d28e0e59af4cf7b493e2a662c5caea31d18f59
Nikolay Starikov <nickolay-starikov@ya.ru>
2019-06-16 21:15:13 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -17,7 +17,23 @@ $(document).ready(function(){
 			slidesToShow: 3,
 			slidesToScroll: 1,
 			arrows: true,
-			dots: false
+			dots: false,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				},
+			]
 		});
 	}
 	
